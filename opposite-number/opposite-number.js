1e0857f8f66f1a0803e917f35853d8a973c8928d
Deekshith_Vangala <deekshithreddy22394@gmail.com>
2017-08-02 13:35:26 +0530

    Re-structured folder structure and added new assignments.

--- opposite-number/opposite-number.js
+++
@@ -1,6 +0,0 @@
-function opposite(number){
-    if(!isNaN(number)){
-        number = number * -1;
-		return number;
-    }
-}
--- word-count/word-count.js
+++
@@ -1,4 +0,0 @@
-function countWords(str) {
-    var words = str.match(/\S+/g);
-    return words ? words.length : 0;
-}
