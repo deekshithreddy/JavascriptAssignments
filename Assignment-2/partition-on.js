b6dc3f78213f4647d7755b2842c04c1dfb41b5cd
Deekshith_Vangala <deekshithreddy22394@gmail.com>
2017-08-02 17:54:05 +0530

    Assignment-2/partition-on.js

--- Assignment-2/partition-on.js
+++ Assignment-2/partition-on.js
@@ -1,1 +1,18 @@
 // partition-on
+function partitionOn(pred, items) {
+    var falseArray = [];
+    var trueArray = [];
+    var midPoint;
+    for (i of items) {
+        if (pred(i)) {
+            trueArray.push(i);
+        }
+        else {
+            falseArray.push(i);
+        }
+    }
+    midPoint = falseArray.length;
+    items.length = 0;
+    Array.prototype.push.apply(items, falseArray.concat(trueArray));
+    return midPoint;
+}
